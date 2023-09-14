546ee7bcabdbb494b9c3534354aafd6218924d65
jeannie <jeannie2@users.noreply.github.com>
2023-09-14 17:28:51 +0800

    alt way

--- proxy.js
+++ proxy.js
@@ -11,45 +11,57 @@ async function scrapePage(website, results) {
     const page = await browser.newPage();
     await page.goto(url);
 
-    await page.waitForSelector(linkSelector, { timeout: 10000 });
+    const timer = setTimeout(() => {
+      console.log('No data found. Refreshing in the next interval...');
+      scrapePage(website, results); // Refresh the page
+    }, 5000); // Refresh after 5 seconds
 
-    const elements = await page.$$(titleSelector);
-    const descriptions = await page.$$(descriptionSelector);
-    const hrefValues = await page.$$eval(linkSelector, (elements) => elements.map((element) => element.getAttribute('href')));
+    try {
+      await page.waitForSelector(linkSelector, { timeout: 10000 });
 
-    const data = await Promise.all(
-      elements.map(async (element, index) => {
-        let title = await element.evaluate((el) => el.textContent);
-        let description = await descriptions[index].evaluate((el) => el.textContent);
-        let hrefValue = hrefValues[index];
-        let opportunityLink = mainLink + hrefValue;
+      clearTimeout(timer); // Clear the timer if data is found
 
-        if (title === '') {
-          title = 'No title available';
-        }
+      const elements = await page.$$(titleSelector);
+      const descriptions = await page.$$(descriptionSelector);
+      const hrefValues = await page.$$eval(linkSelector, (elements) => elements.map((element) => element.getAttribute('href')));
 
-        if (description === '') {
-          description = 'No description available';
-        }
+      const data = await Promise.all(
+        elements.map(async (element, index) => {
+          let title = await element.evaluate((el) => el.textContent);
+          let description = await descriptions[index].evaluate((el) => el.textContent);
+          let hrefValue = hrefValues[index];
+          let opportunityLink = mainLink + hrefValue;
 
-        if (opportunityLink === '') {
-          description = 'No link available';
-        }
+          if (title === '') {
+            title = 'No title available';
+          }
 
-        console.log('Title: ', title);
-        console.log('Description:', description);
-        console.log('hrefValue: ', hrefValue);
-        console.log('opportunityLink :', opportunityLink);
+          if (description === '') {
+            description = 'No description available';
+          }
 
-        return { url, source, title, description, opportunityLink };
-      })
-    );
+          if (opportunityLink === '') {
+            description = 'No link available';
+          }
 
-    results.push(...data);
+          console.log('Title: ', title);
+          console.log('Description:', description);
+          console.log('hrefValue: ', hrefValue);
+          console.log('opportunityLink :', opportunityLink);
+
+          return { url, source, title, description, opportunityLink };
+        })
+      );
+
+      results.push(...data);
+    } catch (error) {
+      console.error(`Error scraping page ${url}:`, error);
+      throw error;
+    }
 
     await browser.close();
   } catch (error) {
-    console.error(`Error scraping page ${url}:`, error);
+    console.error(`Error launching browser:`, error);
     throw error;
   }
 }
@@ -92,6 +104,7 @@ async function startScraping() {
 startScraping();
 
 module.exports = { startScraping };
+
 
 // require('dotenv').config()
 // const puppeteer = require('puppeteer')
@@ -185,3 +198,99 @@ module.exports = { startScraping };
 // }
 
 // module.exports = { startScraping }
+
+
+// require('dotenv').config();
+// const puppeteer = require('puppeteer');
+
+// async function scrapePage(website, results) {
+//   const { url, source, titleSelector, descriptionSelector, linkSelector, mainLink } = website;
+//   console.log(url, source, titleSelector, descriptionSelector, linkSelector, mainLink);
+
+//   try {
+//     const browser = await puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BROWSERLESS_TOKEN}` });
+
+//     const page = await browser.newPage();
+//     await page.goto(url);
+
+//     await page.waitForSelector(linkSelector, { timeout: 10000 });
+
+//     const elements = await page.$$(titleSelector);
+//     const descriptions = await page.$$(descriptionSelector);
+//     const hrefValues = await page.$$eval(linkSelector, (elements) => elements.map((element) => element.getAttribute('href')));
+
+//     const data = await Promise.all(
+//       elements.map(async (element, index) => {
+//         let title = await element.evaluate((el) => el.textContent);
+//         let description = await descriptions[index].evaluate((el) => el.textContent);
+//         let hrefValue = hrefValues[index];
+//         let opportunityLink = mainLink + hrefValue;
+
+//         if (title === '') {
+//           title = 'No title available';
+//         }
+
+//         if (description === '') {
+//           description = 'No description available';
+//         }
+
+//         if (opportunityLink === '') {
+//           description = 'No link available';
+//         }
+
+//         console.log('Title: ', title);
+//         console.log('Description:', description);
+//         console.log('hrefValue: ', hrefValue);
+//         console.log('opportunityLink :', opportunityLink);
+
+//         return { url, source, title, description, opportunityLink };
+//       })
+//     );
+
+//     results.push(...data);
+
+//     await browser.close();
+//   } catch (error) {
+//     console.error(`Error scraping page ${url}:`, error);
+//     throw error;
+//   }
+// }
+
+// async function startScraping() {
+//   const websites = [
+//     {
+//       url: 'https://timeauction.org/en/projects?q%5Bby_skill_categories%5D%5B%5D=52&q%5Bby_skill_categories%5D%5B%5D=48&q%5Bby_volunteer_position_or_details_or_location%5D=&page=1',
+//       source: 'Time Auction',
+//       titleSelector: 'h5.project-card_content-title',
+//       descriptionSelector: 'p.project-card_hover-preview-org',
+//       linkSelector: 'a[href^="/en/project/"]',
+//       mainLink: 'https://www.timeauction.org',
+//     },
+//     {
+//       url: 'https://www.taprootplus.org/opportunities?utf8=%E2%9C%93&search%5Bkeyword%5D=&search%5Bscope%5D=all&search%5Btype%5D=all&search%5Bsort_by%5D=recent&search%5Bcategories%5D%5B%5D=20&page=1',
+//       source: 'Taproot Foundation',
+//       titleSelector: 'h4.opportunity-content__title',
+//       descriptionSelector: 'h3.opportunity-content__organization',
+//       linkSelector: 'a.opportunity-content',
+//       mainLink: 'https://www.taprootplus.org',
+//     },
+//   ];
+
+//   const results = [];
+
+//   for (const website of websites) {
+//     await scrapePage(website, results);
+//   }
+
+//   if (results.length === 0) {
+//     console.log('No data found. Refreshing in the next interval...');
+//     setTimeout(startScraping, 5000); // Refresh after 5 seconds
+//   } else {
+//     console.log('Scraping completed. No further refresh needed.');
+//   }
+// }
+
+// // Call the startScraping function initially
+// startScraping();
+
+// module.exports = { startScraping };
